953639d0db1c12f6c5f046cee3dee0ba584e4d7d
xianda.zheng <xianda.zheng@email.com>
2021-02-24 13:48:21 +0800

    【pod-pipeline】更新文档

--- lib/pod-pipeline.rb
+++ lib/pod-pipeline.rb
@@ -1,15 +1,16 @@
 require 'pod-pipeline/scan'
 
 class PodPipeline
-  # Scan local files!
-  #
-  # Example:
-  #   >> PodPipeline.scan("all")
-  #   => podgitworkspace
-  #
-  # Arguments:
-  #   language: (String)
-  
+    #
+    # Scan local files!
+    #
+    # == Example:
+    #   >> PodPipeline.scan("all")
+    #   => podgitworkspace
+    # @param [String] media 选择扫描内容
+    #
+    # @return String 扫描结果
+    #
     def self.scan(media)
         scan = Scan.new
         case media
