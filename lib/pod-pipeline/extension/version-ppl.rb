38e7ec73cee3aff85d493490a4094bd5e21dded8
ZhengXianda <xianda.zheng@jiamiantech.com>
2021-09-26 17:48:47 +0800

    【update】修复 pod版本号为 1.0.0 时版本号识别错误

--- lib/pod-pipeline/extension/version-ppl.rb
+++ lib/pod-pipeline/extension/version-ppl.rb
@@ -2,17 +2,29 @@ module Pod
     class Version
         def increase_major
             numeric_segments[0] = numeric_segments[0].to_i + 1
-            archiving(numeric_segments.join('.'))
+            archiving(numeric_joint(numeric_segments))
         end
     
         def increase_minor
             numeric_segments[1] = numeric_segments[1].to_i + 1
-            archiving(numeric_segments.join('.'))
+            archiving(numeric_joint(numeric_segments))
         end
     
         def increase_patch
             numeric_segments[2] = numeric_segments[2].to_i + 1
-            archiving(numeric_segments.join('.'))
+            archiving(numeric_joint(numeric_segments))
+        end
+
+        def numeric_joint(numeric_segments)
+            archive = ""
+            numeric_segments.each {|numeric|
+                if numeric.nil?
+                    numeric = 0
+                end
+                archive += '.' + numeric.to_s
+            }
+            archive.slice!(0)
+            return archive
         end
 
         def archiving(new_version)
